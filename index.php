7bca640ea07e5988e7549b842d862a4241fc6885
ax0705 <nick@ip.kharkov.ua>
2024-12-07 23:53:45 +0200

    Commit

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Nais\Providers\GoIP;
-
-require __DIR__ . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
-
-$obj = new GoIP("https://172.16.0.12", "admin", "password");
-
